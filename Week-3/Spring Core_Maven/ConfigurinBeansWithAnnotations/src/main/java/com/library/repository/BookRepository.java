1273cbbbb390b77dde035df1ac32bdabb21bb387
VIGNESH J <133660156+jvignesh148@users.noreply.github.com>
2025-07-01 18:41:26 +0530

    first commit

---
+++ Week-3/Spring Core_Maven/ConfigurinBeansWithAnnotations/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,13 @@
+package com.library.repository;
+
+import org.springframework.stereotype.Repository;
+import java.util.Arrays;
+import java.util.List;
+
+@Repository
+public class BookRepository {
+
+    public List<String> getBooks() {
+        return Arrays.asList("Clean Code", "Spring Boot in Action", "Java Concurrency in Practice");
+    }
+}
