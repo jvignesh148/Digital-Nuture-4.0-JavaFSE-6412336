4787a955e24eaf1423230d7b836ddb27499f2186
VIGNESH J <133660156+jvignesh148@users.noreply.github.com>
2025-07-03 23:42:11 +0530

    first commit

---
+++ Week-3/spring-data-jpa-handson/JPAHibernateSpringDataJPA/src/test/java/com/cognizant/JPAHibernateSpringDataJPA/JpaHibernateSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.JPAHibernateSpringDataJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHibernateSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
