311832a4e6c4e220474914725d81da5bcd4a0cee
VIGNESH J <133660156+jvignesh148@users.noreply.github.com>
2025-07-10 21:55:25 +0530

    first commit

---
+++ Week-4/Spring REST/CountryBasedOnCountryCode/src/test/java/com/cognizant/CountryBasedOnCountryCode/CountryBasedOnCountryCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.CountryBasedOnCountryCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryBasedOnCountryCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
